a5563fb95300d5e548df6ada087cc843957f41ac
Jugal Shrestha <jugalnepali@gmail.com>
2023-09-02 09:25:57 +0545

    Remove: Base.jsx

--- src/assets/components/project_data.jsx
+++ src/assets/components/project_data.jsx
@@ -1,12 +1,10 @@
-import base from "./base"
-
 const project_data = [
     {
         name: "nischal malakar",
         desc: "A digital marketer based in Dubai,The portfolio was made on the basis of the client’s need. The portfolio contains some of the brands he has worked with and the skills plus services that he offers!",
         techStack: "Html, Scss, Javascript",
         link: "https://www.nischalmalakar.com.np/",
-        photo: base+"/images/nischal-malakar-portfolio-sample.png",
+        photo: "/images/nischal-malakar-portfolio-sample.png",
     }
     ,
     {
@@ -14,7 +12,7 @@ const project_data = [
         desc: "an artist specialized in thanka who does a bit of everything. an artist who can paint beautiful modern or abstract themed paintings. and also an artist who is an art teacher teaching her life long. she inspires thousands of her followers to be creative and inspiring. she tells us about how she has followed her dream work and is still inspiring thousand others to follow their dream work no matter how the society percieves it."
         ,techStack: "Html, Scss, Javascript",
         link: "https://www.junumali.com.np/",
-        photo: base+"/images/nischal-malakar-portfolio-sample.png",
+        photo: "/images/nischal-malakar-portfolio-sample.png",
     }
     ,
     {
@@ -22,14 +20,14 @@ const project_data = [
         desc: "A website that was built for my college  which can do your homeworks, make generation of a cover page for your assignments according the standards of my college, and also provide you with notes that are relevant to you!",
         techStack: "TypeScript, React, Scss",
         link: "https://jugalshrestha.github.io/bim21/",
-        photo: base+"/images/bim21-sample.png",
+        photo: "/images/bim21-sample.png",
     },
     {
         name: "Algo Visualizer",
         desc: " it will make you understand the working of sorting algorithms with visualization of the actual process. Bubble sort, selection sort, insertion sort, etc.",
         techStack: "JavaScript, HTML, SCSS",
         link: "https://jugalshrestha.github.io/sorting-algorithm-visualizer/",
-        photo: base+ "/images/algo-visualizer-sample.png",
+        photo:  "/images/algo-visualizer-sample.png",
     }
 ]
 
