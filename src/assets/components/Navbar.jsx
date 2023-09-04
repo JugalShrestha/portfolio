329be75c123f8e6051c98891bbb00452243a4df8
Jugal Shrestha <jugalnepali@gmail.com>
2023-09-04 11:54:40 +0545

    Fix: Minor Design issues

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -42,15 +42,16 @@ const Navbar = React.forwardRef(({scrollToComponent},ref) => {
     const checkScroll = () => {
       if (!isScrolling) {
         // Add a buffer of a few pixels around 99 scrollY
-        if (window.scrollY >= 100) {
+        if (window.scrollY >= 200) {
           setScrolled(true);
-        } else {
+        }
+        else {
           setScrolled(false);
         }
         isScrolling = true;
         setTimeout(() => {
           isScrolling = false;
-        }, 100); // Adjust this delay as needed
+        }, 100);
       }
     };
 
@@ -68,7 +69,7 @@ const Navbar = React.forwardRef(({scrollToComponent},ref) => {
         <div className="nav-options">
             <div onClick={handleClick(scrollToComponent.workPageRef)} className="nav-option">works</div>
             <div onClick={handleClick(scrollToComponent.contactPageRef)} className="nav-option">contact</div>
-            <div className="nav-option"><img onClick={handleDarkMode} src="/images/light-mode-toggle.svg" alt="mode toggle" /></div>    
+            <div onClick={handleDarkMode} className="nav-option"> <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" viewBox="0 0 312.999 312.999"><g><path d="M305.6 178.053c-3.2-.8-6.4 0-9.2 2-10.4 8.8-22.4 16-35.6 20.8-12.4 4.8-26 7.2-40.4 7.2-32.4 0-62-13.2-83.2-34.4-21.2-21.2-34.4-50.8-34.4-83.2 0-13.6 2.4-26.8 6.4-38.8 4.4-12.8 10.8-24.4 19.2-34.4 3.6-4.4 2.8-10.8-1.6-14.4-2.8-2-6-2.8-9.2-2-34 9.2-63.6 29.6-84.8 56.8-20.4 26.8-32.8 60-32.8 96.4 0 43.6 17.6 83.2 46.4 112s68.4 46.4 112 46.4c36.8 0 70.8-12.8 98-34 27.6-21.6 47.6-52.4 56-87.6 2-6-1.2-11.6-6.8-12.8zm-61.2 83.6c-23.2 18.4-52.8 29.6-85.2 29.6-38 0-72.4-15.6-97.2-40.4-24.8-24.8-40.4-59.2-40.4-97.2 0-31.6 10.4-60.4 28.4-83.6 12.4-16 28-29.2 46-38.4-2 4.4-4 8.8-5.6 13.6-5.2 14.4-7.6 29.6-7.6 45.6 0 38 15.6 72.8 40.4 97.6s59.6 40.4 97.6 40.4c16.8 0 32.8-2.8 47.6-8.4 5.2-2 10.4-4 15.2-6.4-9.6 18.4-22.8 34.8-39.2 47.6z" ></path></g></svg></div>    
         </div>
     </div>
   )
